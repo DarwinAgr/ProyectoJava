b5c0ccf5fab9d672982c14c4c3b4113dba1c45ba
Darwin Aguilar <darwinaguilar04@outlook.com>
2025-06-16 23:15:57 -0600

    Interfaz y clase de NivelUsuarioWriteForm

---
+++ src/main/java/Esfe/Presentacion/MainForm.java
@@ -0,0 +1,6 @@
+package Esfe.Presentacion;
+
+import javax.swing.*;
+
+public class MainForm extends JFrame {
+}
---
+++ src/main/java/Esfe/Presentacion/NivelUsuarioReadingForm.java
@@ -0,0 +1,13 @@
+package Esfe.Presentacion;
+
+import javax.swing.*;
+
+public class NivelUsuarioReadingForm extends JDialog{
+    private JPanel mainPanel;
+    private JButton btnCreate;
+    private JTable tbUsuarios;
+    private JButton btnUpdate;
+    private JButton btnDelete;
+    private JLabel nombre;
+    private JTextField txtName;
+}
